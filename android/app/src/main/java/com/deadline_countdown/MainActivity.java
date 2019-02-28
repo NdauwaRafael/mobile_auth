56a7ec5be9df464b533134c6fb97217832c4c515
Raphael Karanja <rkaranja@cytonn.com>
2019-02-28 22:16:30 +0300

    add files

---
+++ android/app/src/main/java/com/deadline_countdown/MainActivity.java
@@ -0,0 +1,15 @@
+package com.deadline_countdown;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "deadline_countdown";
+    }
+}
