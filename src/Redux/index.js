a800c2fd60aec26b746fb30bfd3fb94c066c7807
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 12:16:59 +0300

    UPDATE:  configure store file to add various routes

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,3 +1,25 @@
 /**
  * Created by Raphael Karanja on 2019-03-03.
  */
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+
+// Navigation
+import { EventsTabNavigator } from '../App/eventsTab/navigationConfigutation';
+import { ProfileTabNavigator } from '../App/profileTab/navigationConfigutation';
+import { TabBar } from '../App/tabBar/navigationConfiguration';
+
+// Middleware
+const middleware = () => {
+    return applyMiddleware(logger())
+}
+
+
+export default createStore(
+    combineReducers({
+        tabBar: (state,action) => TabBar.router.getStateForAction(action,state),
+        eventsTab: (state,action) => EventsTabNavigator.router.getStateForAction(action,state),
+        profileTab: (state,action) => ProfileTabNavigator.router.getStateForAction(action,state),
+    }),
+    middleware(),
+)
