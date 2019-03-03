21f69faa0d5122b21718159fe93c5cbf39c2cb86
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 12:08:10 +0300

    UPDATE:  add navigation configurations for tabs component

--- src/App/profileTab/navigationConfigutation.js
+++ src/App/profileTab/navigationConfigutation.js
@@ -1,3 +1,20 @@
 /**
  * Created by Raphael Karanja on 2019-03-03.
  */
+import { createStackNavigator } from 'react-navigation';
+
+//SCREENS
+import ProfileDetails from './views/ProfileDetails';
+import EditProfile from  './views/ProfileEdit'
+
+const routeConfiguration = {
+    ProfileDetails: { screen: ProfileDetails },
+    EventsForm: { screen: EditProfile }
+}
+const stackNavigatorConfiguration = {
+    headerMode: 'none',
+    initialRouteName: 'ProfileDetails'
+};
+
+
+export const ProfileTabNavigator = createStackNavigator(routeConfiguration,stackNavigatorConfiguration)
