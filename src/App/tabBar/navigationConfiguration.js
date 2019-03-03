4d4a687cc4315f2feb6af065d2e31502fe6fe8f7
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 11:53:39 +0300

    UPDATE:  import various pages navigation tab configurations and create a configuration file with the various screens

--- src/App/tabBar/navigationConfiguration.js
+++ src/App/tabBar/navigationConfiguration.js
@@ -2,3 +2,11 @@
  * Created by Raphael Karanja on 2019-03-03.
  */
 import { TabNavigator } from 'react-navigation';
+
+import EventsTabNavigation from '../eventsTab/views/EventsTabNavigation';
+import ProfileTabNavigation from  '../profileTab/views/ProfileTabNavigation';
+
+const routeConfiguration = {
+    Events: { screen: EventsTabNavigation },
+    Profile: { screen: ProfileTabNavigation }
+}
