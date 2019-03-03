cf5c42606f7a743998ce1da073bd2da7dbd64f17
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 12:40:42 +0300

    UPDATE:  update logger import

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -2,7 +2,7 @@
  * Created by Raphael Karanja on 2019-03-03.
  */
 import { applyMiddleware, combineReducers, createStore } from 'redux';
-import logger from 'redux-logger';
+import {createLogger} from 'redux-logger';
 
 // Navigation
 import { EventsTabNavigator } from '../App/eventsTab/navigationConfigutation';
@@ -11,7 +11,7 @@ import { TabBar } from '../App/tabBar/navigationConfiguration';
 
 // Middleware
 const middleware = () => {
-    return applyMiddleware(logger())
+    return applyMiddleware(createLogger())
 }
 
 
