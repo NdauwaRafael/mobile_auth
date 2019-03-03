1daf00f3d6f693dd933d36f2316106197af42cf0
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 12:38:29 +0300

    UPDATE:  add configurations for profiletabnavigation

--- src/App/profileTab/views/ProfileTabNavigation.js
+++ src/App/profileTab/views/ProfileTabNavigation.js
@@ -1,3 +1,46 @@
 /**
  * Created by Raphael Karanja on 2019-03-03.
  */
+
+// React
+import React from 'react'
+
+// Navigation
+import { ProfileTabNavigator} from '../navigationConfigutation'
+
+// Redux
+import { connect } from 'react-redux'
+
+// Icon
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+
+class ProfileTabNavigation extends React.Component {
+
+    static navigationOptions = {
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ tintColor }) => <Icon size={ 20 } name={ 'cogs' } color={ tintColor }/>
+    }
+
+    render(){
+        const { navigationState, dispatch } = this.props;
+        return (
+            <ProfileTabNavigator
+                navigation={
+                    {
+                        dispatch: dispatch,
+                        state: navigationState
+                    }
+                }
+            />
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        navigationState: state.profileTab
+    }
+}
+
+export default connect(mapStateToProps)(ProfileTabNavigation);
