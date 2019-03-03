97628050733bdd9c66e5c471926ca1596592c047
Raphael Karanja <rkaranja@cytonn.com>
2019-03-03 12:06:25 +0300

    UPDATE:  complete eventstab configuration

--- src/App/eventsTab/navigationConfigutation.js
+++ src/App/eventsTab/navigationConfigutation.js
@@ -1,3 +1,22 @@
 /**
  * Created by Raphael Karanja on 2019-03-03.
  */
+import { createStackNavigator } from 'react-navigation';
+
+//SCREENS
+import Event from './views/Event';
+import EventsForm from  './views/EventsForm';
+import EventsList from  './views/EventsList';
+
+const routeConfiguration = {
+    EventsList: { screen: EventsList },
+    EventsForm: { screen: EventsForm },
+    Event: { screen: Event },
+}
+const stackNavigatorConfiguration = {
+    headerMode: 'none',
+    initialRouteName: 'EventsList'
+};
+
+
+export const EventsTabNavigator = createStackNavigator(routeConfiguration,stackNavigatorConfiguration)
