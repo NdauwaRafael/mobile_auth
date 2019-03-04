b305d83751de2b13dbc78bfc63b21be2bedba04c
Raphael Karanja <rkaranja@cytonn.com>
2019-03-04 23:42:29 +0300

    UPDATE:  add action to add an event

--- src/Redux/actions/Events/index.js
+++ src/Redux/actions/Events/index.js
@@ -1,3 +1,16 @@
 /**
  * Created by Raphael Karanja on 2019-03-04.
  */
+import {ADDED_AN_EVENT } from "../../constants/ActionTypes";
+
+export const addEventSuccess = (resp) =>{
+    return {
+        type: ADDED_AN_EVENT,
+        events: resp
+    }
+}
+
+export const addEvent = (event)=>dispatch=> {
+
+    return dispatch(addEventSuccess(event));
+}
