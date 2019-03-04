b3e88ff4c2f982c4dc14561b3d743bd985f2b637
Raphael Karanja <rkaranja@cytonn.com>
2019-03-04 21:59:57 +0300

    UPDATE:  downgrade dependencies

--- android/app/src/main/java/com/deadline_countdown/MainApplication.java
+++ android/app/src/main/java/com/deadline_countdown/MainApplication.java
@@ -3,6 +3,8 @@ package com.deadline_countdown;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNGestureHandlerPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage()
       );
