fda0a31859a82a84ac3e1b27e7f3398e0f71d643
Raphael Karanja <rkaranja@cytonn.com>
2019-03-04 23:49:11 +0300

    DEBUG: add clpsing parenthesis for mapDispatch method

--- src/App/eventsTab/views/EventsList.js
+++ src/App/eventsTab/views/EventsList.js
@@ -1,42 +1,42 @@
-
 /**
  * Created by Raphael Karanja on 2019-03-03.
  */
 
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
 import * as eventsActions from '../../../Redux/actions/Events'
 import {bindActionCreators} from "redux";
+
 class EventsList extends React.Component {
-    render(){
-        return(
+    render() {
+        return (
             <View style={{
-                flex:1,
-                backgroundColor:'red',
-                alignItems:'center',
-                justifyContent:'center'
+                flex: 1,
+                backgroundColor: 'red',
+                alignItems: 'center',
+                justifyContent: 'center'
             }}>
-                <Text>{ 'EventsList Screen ' }</Text>
+                <Text>{'EventsList Screen '}</Text>
                 <TouchableOpacity
-                    onPress={ () => this.props.actions.addEvent('Hello World') }
+                    onPress={() => this.props.actions.addEvent('Hello World')}
                     style={{
-                        padding:20,
-                        borderRadius:20,
-                        backgroundColor:'green',
-                        marginTop:20,
+                        padding: 20,
+                        borderRadius: 20,
+                        backgroundColor: 'green',
+                        marginTop: 20,
                         color: 'white'
                     }}>
                     <Text>{'Add an Event'}</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                    onPress={ () => this.props.navigation.navigate('EventsForm') }
+                    onPress={() => this.props.navigation.navigate('EventsForm')}
                     style={{
-                        padding:20,
-                        borderRadius:20,
-                        backgroundColor:'purple',
-                        marginTop:20
+                        padding: 20,
+                        borderRadius: 20,
+                        backgroundColor: 'purple',
+                        marginTop: 20
                     }}>
                     <Text>{'Go to Event Form screen'}</Text>
                 </TouchableOpacity>
@@ -45,10 +45,10 @@ class EventsList extends React.Component {
     }
 }
 
-const mapDispatchToProps = (dispatch)=>{
+const mapDispatchToProps = (dispatch) => {
     return {
         actions: bindActionCreators(eventsActions, dispatch)
-
     }
+}
 
 export default connect(mapDispatchToProps)(EventsList)
