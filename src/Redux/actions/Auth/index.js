056f91acc336b1e5027a521aa4436caa2595e979
Raphael Karanja <rkaranja@cytonn.com>
2019-03-05 20:24:08 +0300

    UPDATE:  import actionTypes in auth action page

--- src/Redux/actions/Auth/index.js
+++ src/Redux/actions/Auth/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by Raphael Karanja on 2019-03-05.
  */
+import {
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAILED,
+    CREATE_USER_SUCCESS,
+    CREATE_USER_FAILED,
+    FETCH_USER_SUCCESS,
+    FETCH_USER_FAILED,
+    LOGOUT_USER_SUCCESS,
+    LOGOUT_USER_FAILED
+}   from '../../constants/ActionTypes'
