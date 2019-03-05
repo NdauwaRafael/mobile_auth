12e622b451fbd236c7073d3227c39c3bd05299d0
Raphael Karanja <rkaranja@cytonn.com>
2019-03-05 19:50:30 +0300

    UPDATE:  add component boiler plate for login page

--- src/App/authTab/views/Login/index.js
+++ src/App/authTab/views/Login/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by Raphael Karanja on 2019-03-05.
  */
+import React, {Component} from 'react';
+import {Container, Header, Content, Footer, Title} from 'native-base';
+
+class Login extends Component{
+        render() {
+            return(
+                <Container>
+                    
+                </Container>
+            );
+        }
+}
+
+export default Login;
