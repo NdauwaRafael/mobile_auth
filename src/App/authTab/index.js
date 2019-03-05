8be50716f811196a5eb3ce8baa94818b2370bb3f
Raphael Karanja <rkaranja@cytonn.com>
2019-03-05 20:05:39 +0300

    UPDATE:  import dependencies for authTab routes

--- src/App/authTab/index.js
+++ src/App/authTab/index.js
@@ -1,3 +1,12 @@
 /**
  * Created by Raphael Karanja on 2019-03-05.
  */
+//REACT
+import React from "react";
+
+//ICON
+import Icon from "react-native-vector-icons/FontAwesome";
+
+//SCREENS
+import Login from './views/Login';
+import Register from './views/Register';
