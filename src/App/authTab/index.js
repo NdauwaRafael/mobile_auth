f87620ee9fe8038cca9a1dd4182797bb04c9992e
Raphael Karanja <rkaranja@cytonn.com>
2019-03-05 20:07:57 +0300

    UPDATE:  add AuthTab navigation configuration

--- src/App/authTab/index.js
+++ src/App/authTab/index.js
@@ -3,6 +3,8 @@
  */
 //REACT
 import React from "react";
+import {createStackNavigator} from "react-navigation";
+
 
 //ICON
 import Icon from "react-native-vector-icons/FontAwesome";
@@ -10,3 +12,17 @@ import Icon from "react-native-vector-icons/FontAwesome";
 //SCREENS
 import Login from './views/Login';
 import Register from './views/Register';
+
+const routeConfiguration = {
+    Login: {screen: Login},
+    Register: {screen: Register}
+}
+
+const stackNavigatorConfiguration = {
+    headerMode: 'none',
+    initialRouteName: 'Login',
+    tabBarLabel: 'Auth',
+    tabBarIcon: ({ tintColor }) => <Icon size={ 20 } name={ 'cogs' } color={ tintColor }/>
+};
+
+export const AuthTabNavigator = createStackNavigator(routeConfiguration,stackNavigatorConfiguration);
