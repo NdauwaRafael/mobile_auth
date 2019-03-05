c0041d66924e6f48b645f9615dde3ab8343ce150
Raphael Karanja <rkaranja@cytonn.com>
2019-03-05 20:36:56 +0300

    UPDATE:  dispatch action checkLoginState in root file

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ import {name as appName} from './app.json';
 // Redux
 import { Provider } from 'react-redux'
 import store from './src/Redux';
+import {checkLoginState} from "./src/Redux/actions/Auth";
+
+store.dispatch(checkLoginState());
+
 
 // Navigation
 import TabBarNavigation from './src/App/tabBar';
