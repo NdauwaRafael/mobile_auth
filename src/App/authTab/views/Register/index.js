2b8514180cd569c03a3634d0503329a482cbe9db
Raphael Karanja <rkaranja@cytonn.com>
2019-03-06 17:17:43 +0300

    UPDATE:  add state for register component

--- src/App/authTab/views/Register/index.js
+++ src/App/authTab/views/Register/index.js
@@ -2,15 +2,32 @@
  * Created by Raphael Karanja on 2019-03-05.
  */
 import React, {Component} from 'react';
-import {Container, Header, Content, Footer, Title} from 'native-base';
+import {Container, Header, Form, Footer, Title, View, Item, Label, Input, Text} from 'native-base';
 
 class Register extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
     render() {
         return(
             <Container>
-                <Content>
-
-                </Content>
+                <Form>
+                    <View>
+                        <Item floatingLabel>
+                            <Label>Email </Label>
+                            <Input
+                                onChangeText={(email) => this.setState({email})}
+                                autoCorrect={false}
+                                autoCapitalize="none"/>
+                        </Item>
+                        <Text style={styles.errorMessage}>{errors.email} </Text>
+                    </View>
+                </Form>
             </Container>
         );
     }
