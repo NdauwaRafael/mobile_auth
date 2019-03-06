300e5f06a67a9150076818b9f6b4c31695cfafb4
Raphael Karanja <rkaranja@cytonn.com>
2019-03-06 18:09:15 +0300

    UPDATE:  add basic boiler plate for the component

--- src/App/authTab/views/UpdateUser/index.js
+++ src/App/authTab/views/UpdateUser/index.js
@@ -1,3 +1,21 @@
 /**
  * Created by Raphael Karanja on 2019-03-06.
  */
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Container} from 'native-base'
+class UpdateUser extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return(
+            <Container>
+
+            </Container>
+        );
+    }
+}
+
+export default connect()(UpdateUser);
