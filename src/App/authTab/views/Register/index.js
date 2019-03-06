69e9793c9fee2d5b6234db7e937dfbbbc311792f
Raphael Karanja <rkaranja@cytonn.com>
2019-03-06 17:20:37 +0300

    UPDATE:  add register user form

--- src/App/authTab/views/Register/index.js
+++ src/App/authTab/views/Register/index.js
@@ -2,7 +2,7 @@
  * Created by Raphael Karanja on 2019-03-05.
  */
 import React, {Component} from 'react';
-import {Container, Header, Form, Footer, Title, View, Item, Label, Input, Text} from 'native-base';
+import {Container, Header, Form, Footer, Title, View, Item, Label, Input, Text, Button} from 'native-base';
 
 class Register extends Component{
     constructor(props){
@@ -10,10 +10,16 @@ class Register extends Component{
         this.state = {
             email: '',
             password: '',
-            confirmPassword: ''
+            confirmPassword: '',
+            errors: {
+                email: '',
+                password: '',
+                confirmPassword: ''
+            }
         }
     }
     render() {
+        const {errors} = this.state;
         return(
             <Container>
                 <Form>
@@ -27,6 +33,39 @@ class Register extends Component{
                         </Item>
                         <Text style={styles.errorMessage}>{errors.email} </Text>
                     </View>
+
+                    <View>
+                        <Item floatingLabel>
+                            <Label>Password</Label>
+                            <Input
+                                onChangeText={(password) => this.setState({password})}
+                                secureTextEntry={true}
+                                autoCorrect={false}
+                                autoCapitalize="none"/>
+                        </Item>
+                        <Text style={styles.errorMessage}>{errors.password} </Text>
+                    </View>
+
+                    <View>
+                        <Item floatingLabel>
+                            <Label>Confirm Password</Label>
+                            <Input
+                                onChangeText={(confirmPassword) => this.setState({confirmPassword})}
+                                secureTextEntry={true}
+                                autoCorrect={false}
+                                autoCapitalize="none"/>
+                        </Item>
+                        <Text style={styles.errorMessage}>{errors.password} </Text>
+                    </View>
+
+                    <Button
+                        style={{marginTop: 15}}
+                        full
+                        rounded
+                        success
+                        onPress={() => this.register()}>
+                        <Text style={{color: '#fff'}}>Register User</Text>
+                    </Button>
                 </Form>
             </Container>
         );
